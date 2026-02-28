e6b7f67efe117fa705fdec2c90ed7249a65aed0a
SOURAVBHOWAL <121746098+SOURAVBHOWAL@users.noreply.github.com>
2026-02-28 15:03:53 +0530

    28022026

---
+++ EmployeeProject/src/main/java/com/model/Employee.java
@@ -0,0 +1,47 @@
+package com.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Employee {
+	@Id
+	@Column
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private double salary;
+	public Employee() {
+		
+	}
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return id+" "+name+" "+salary;
+	}
+	
+}
