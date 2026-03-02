121afa2e5fb336048872fbd93e1227bfbe984276
SOURAVBHOWAL <121746098+SOURAVBHOWAL@users.noreply.github.com>
2026-03-02 21:15:52 +0530

    Delete Demo2 directory

--- Demo2/src/CodeTest/InterfaceConcrete/Parent.java
+++
@@ -1,8 +0,0 @@
-package CodeTest.InterfaceConcrete;
-
-public interface Parent {
-	public static final int var=10;
-	public static void m1() {
-		System.out.println("inside parent interface");
-	}
-}
